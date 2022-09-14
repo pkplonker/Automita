780c9a7cde96670641a142a6cd227dff70cddb28
Stuart Heath <heaths8@uni.coventry.ac.uk>
2022-09-14 16:27:59 +0100

    sh alignment

---
+++ Assets/Scripts/SaveFileHandler.cs
@@ -0,0 +1,15 @@
+﻿
+using System.IO;
+using UnityEngine;
+
+public class SaveFileHandler 
+{
+	public void Save()
+	{
+		const string dir = "/SaveData/";
+		var path = Application.persistentDataPath + dir + ".txt";
+		if (!Directory.Exists(Application.persistentDataPath + dir)) Directory.CreateDirectory(dir);
+		//var json = JsonUtility.ToJson();
+		//File.WriteAllText(path, json);
+	}
+}
