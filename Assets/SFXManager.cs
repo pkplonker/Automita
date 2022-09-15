1a89c302c9758626009f17612a07f168a1b2d438
Akhilesh Luchmun <luchmuna@coventry.ac.uk>
2022-09-15 17:41:01 +0100

    I have added the music and sfx slider functionally and updated credits

---
+++ Assets/SFXManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SFXManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("sfxVolume"))
+        {
+            PlayerPrefs.SetFloat("sfxVolume", 1);
+            Load();
+        }
+
+        else
+        {
+            Load();
+        }
+
+    }
+
+
+    public void ChangeSFXVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("sfxVolume", volumeSlider.value);
+    }
+}
