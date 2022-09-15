4fb4dd893e8de3c31b3544339056b4666bdc9033
stuart <pkplonker@gmail.com>
2022-09-15 21:14:43 +0100

    Sh Small code tidyup

--- Assets/Scripts/Stuart/UFOMove.cs
+++ Assets/Scripts/Stuart/UFOMove.cs
@@ -3,14 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UFOMove : MonoBehaviour
+namespace StuartH
 {
-    [SerializeField] private float y;
-    [SerializeField] private float rotSpeed = 30f;
-    private void Update()
+    /// <summary>
+    ///UFOMove - Moves the UFO
+    /// </summary>
+    public class UFOMove : MonoBehaviour
     {
-        transform.Rotate(transform.up,rotSpeed*Time.deltaTime);
-        y= Mathf.PingPong(Time.time, 0.5f);
-        transform.position = new Vector3(transform.position.x,y+1.5f,transform.position.z) ;
+        [SerializeField] private float y;
+        [SerializeField] private float rotSpeed = 30f;
+
+        private void Update()
+        {
+            transform.Rotate(transform.up, rotSpeed * Time.deltaTime);
+            y = Mathf.PingPong(Time.time, 0.5f);
+            transform.position = new Vector3(transform.position.x, y + 1.5f, transform.position.z);
+        }
     }
 }
--- Assets/Scripts/Stuart/UI/CreditsUI.cs
+++ Assets/Scripts/Stuart/UI/CreditsUI.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using StuartH;
 using UnityEngine;
 
-
-public class CreditsUI : CanvasGroupBase
+namespace StuartH
 {
-    private void Awake() => Hide();
-    public void Show() => ShowUI();
-    public void Hide() => HideUI();
+    /// <summary>
+    ///CreditsUI - This class is responsible for displaying the credits screen
+    /// </summary>
+    public class CreditsUI : CanvasGroupBase
+    {
+        private void Awake() => Hide();
+        public void Show() => ShowUI();
+        public void Hide() => HideUI();
 
+    }
 }
