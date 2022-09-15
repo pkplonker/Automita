7a48aa3c267fff2d253f7cc202fd63beaab88689
Stuart Heath <heaths8@uni.coventry.ac.uk>
2022-09-15 15:14:04 +0100

    SH main menu changes

---
+++ Assets/Scripts/Stuart/UFOMove.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOMove : MonoBehaviour
+{
+    [SerializeField] private float y;
+    [SerializeField] private float rotSpeed = 30f;
+    private void Update()
+    {
+        transform.Rotate(transform.up,rotSpeed*Time.deltaTime);
+        y= Mathf.PingPong(Time.time, 0.5f);
+        transform.position = new Vector3(transform.position.x,y+1.5f,transform.position.z) ;
+    }
+}
