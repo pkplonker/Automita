52f122a97ff4c44b33ddb27895c7fd5cf4f5501e
Stuart Heath <heaths8@uni.coventry.ac.uk>
2022-09-16 11:57:48 +0100

    SH hs image change

--- Assets/Scripts/Stuart/SaveFileHandler.cs
+++ Assets/Scripts/Stuart/SaveFileHandler.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.IO;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 namespace StuartH
 {
@@ -17,29 +15,17 @@ namespace StuartH
 
 		public static void Save(ScoreHolder scoreHolder)
 		{
-			Debug.Log("Trying to save to" + GetPathWithFile());
-
 			var json = JsonUtility.ToJson(scoreHolder.scores);
 			File.WriteAllText(GetPathWithFile(), json);
-			Debug.Log("Saved file");
-
 		}
 
 		public static void Load(ScoreHolder scoreHolder)
 		{
-			Debug.Log("Trying to load from" + GetPathWithFile());
-			if (!Directory.Exists(GetPath()))
-			{
-				Debug.Log("failed to access dir");
-				return ;
-			}
-
+			if (!Directory.Exists(GetPath())) return ;
 			if (!File.Exists(GetPathWithFile())) return ;
 			var x = File.ReadAllText(GetPathWithFile());
 			var sh = JsonUtility.FromJson<ScoreHolder.ScoreSaveDataList>(x);
 			scoreHolder.LoadScores(sh);
-			Debug.Log("Loaded file");
-			return ;
 		}
 	}
 
